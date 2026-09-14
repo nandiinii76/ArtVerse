24d9dc701ae4e87d5e28186ffeec330c8084a76a
NANDINI <nandiniboddupallii@gmail.com>
2026-09-14 17:47:50 +0530

    feat: enable scheduled auction lifecycle jobs

--- backend/src/main/java/com/artverse/ArtverseApplication.java
+++ backend/src/main/java/com/artverse/ArtverseApplication.java
@@ -2,8 +2,10 @@ package com.artverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ArtverseApplication {
 
     public static void main(String[] args) {
