aa26fb32b95f0ecea8fe7b285593b8df219521fe
NANDINI <nandiniboddupallii@gmail.com>
2026-09-14 17:34:49 +0530

    feat: add marketplace payment notifications

--- backend/src/main/java/com/artverse/notification/NotificationEventService.java
+++ backend/src/main/java/com/artverse/notification/NotificationEventService.java
@@ -1,6 +1,5 @@
 package com.artverse.notification;
 
-import com.artverse.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,27 +11,19 @@ public class NotificationEventService {
     private final NotificationService notifications;
 
     public void favorite(UUID artistId, UUID actorId, String artworkTitle) {
-        if (!same(artistId, actorId)) {
-            notifications.create(artistId, "ARTWORK_FAVORITED", "Artwork saved", "Someone saved your artwork \"" + artworkTitle + "\".");
-        }
+        if (!same(artistId, actorId)) notifications.create(artistId, "ARTWORK_FAVORITED", "Artwork saved", "Someone saved your artwork \"" + artworkTitle + "\".");
     }
 
     public void comment(UUID artistId, UUID actorId, String artworkTitle) {
-        if (!same(artistId, actorId)) {
-            notifications.create(artistId, "ARTWORK_COMMENTED", "New comment", "Someone commented on your artwork \"" + artworkTitle + "\".");
-        }
+        if (!same(artistId, actorId)) notifications.create(artistId, "ARTWORK_COMMENTED", "New comment", "Someone commented on your artwork \"" + artworkTitle + "\".");
     }
 
     public void follow(UUID artistId, UUID followerId) {
-        if (!same(artistId, followerId)) {
-            notifications.create(artistId, "NEW_FOLLOWER", "New follower", "Someone started following your work.");
-        }
+        if (!same(artistId, followerId)) notifications.create(artistId, "NEW_FOLLOWER", "New follower", "Someone started following your work.");
     }
 
     public void auctionBid(UUID sellerId, UUID bidderId, String artworkTitle) {
-        if (!same(sellerId, bidderId)) {
-            notifications.create(sellerId, "AUCTION_BID", "New auction bid", "A new bid was placed on \"" + artworkTitle + "\".");
-        }
+        if (!same(sellerId, bidderId)) notifications.create(sellerId, "AUCTION_BID", "New auction bid", "A new bid was placed on \"" + artworkTitle + "\".");
     }
 
     public void auctionWon(UUID winnerId, String artworkTitle) {
@@ -40,8 +31,13 @@ public class NotificationEventService {
     }
 
     public void marketplaceOrder(UUID sellerId, UUID buyerId, String artworkTitle) {
+        if (!same(sellerId, buyerId)) notifications.create(sellerId, "MARKETPLACE_ORDER", "Artwork purchase started", "Someone started an order for \"" + artworkTitle + "\".");
+    }
+
+    public void marketplaceOrderPaid(UUID sellerId, UUID buyerId, String artworkTitle) {
         if (!same(sellerId, buyerId)) {
-            notifications.create(sellerId, "MARKETPLACE_ORDER", "Artwork purchase started", "Someone started an order for \"" + artworkTitle + "\".");
+            notifications.create(sellerId, "MARKETPLACE_ORDER_PAID", "Artwork sold", "Your artwork \"" + artworkTitle + "\" has been purchased.");
+            notifications.create(buyerId, "MARKETPLACE_PURCHASED", "Purchase confirmed", "Your purchase of \"" + artworkTitle + "\" is complete.");
         }
     }
 
